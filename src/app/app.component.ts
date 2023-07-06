cadebad0c01e3da4d0db4d3d5d283375ac2a87a8
Jhonatan Estiven Becerra Pedrozo <jsbpdeveloper@gmail.com>
2023-07-06 11:23:10 -0500

    Probando metodos signals 16

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, signal, computed, effect } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'prueba-signal';
+  // countSuma = 0;
+  constructor() {
+    effect(() => {
+      console.log('countSumaEfect: ' + this.countSuma());
+    });
+    effect(() => {
+      if (this.count() == 10) {
+        console.log('countEfect: ' + this.count());
+      }
+    });
+  }
+
+  //signals
+  count = signal(1);
+  doubleCount = computed(() => this.count() * 2);
+  countSuma = signal(2);
+  count3 = signal(3);
+
+  //methods
+  triggerChanges() {
+    this.countSuma.set(this.countSuma() + 1);
+  }
+  triggerChanges2() {
+    this.count.update((prev) => prev + 1);
+  }
+
+  calculoComputed = computed(() => {
+    return this.count() >= 10;
+  });
 }
